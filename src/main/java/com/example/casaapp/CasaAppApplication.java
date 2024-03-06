1778bea98ff6f60d3340e48960bf7e0c3461b0a5
Jinsol Lee <jinsollee758@gmail.com>
2024-03-06 23:43:39 +0900

    스프링부트 + 리액트 따로 연동...

---
+++ src/main/java/com/example/casaapp/CasaAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.casaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasaAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/casaapp/CasaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.casaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
